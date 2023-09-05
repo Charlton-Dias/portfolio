3ee6ba235306fd2b222bf2f54ab15c9d9b2eec1e
Charlton Dias <charltondias29@gmail.com>
2023-09-06 02:19:25 +0530

    added education timeline

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,21 +1,54 @@
-import {education} from '../data/education.json';
+import { Card } from 'react-bootstrap';
+import { education } from '../data/education.json';
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineOppositeContent,
+  TimelineDot
+} from '@mui/lab';
 
 export default function Resume() {
   return (
     <>
       <h1>Resume</h1>
       <h2>Education</h2>
-      
-      {education.map((item, index) => {
-        return (
-          <div key={index}>
-            <h3>{item?.institution}</h3>
-            <p>{item?.description}</p>
-            <p>{item?.studyType}</p>
-            <p>{item?.startDate} - {item?.endDate}</p>
-          </div>
-        )
-      })}
+
+      <Timeline position="alternate">
+        {education.map((item, index) => {
+          return (
+            <>
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>{item?.description}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">{item?.institution}</Card.Subtitle>
+                    </Card.Body>
+                  </Card>
+                </TimelineOppositeContent>
+
+                <TimelineSeparator>
+                  <TimelineDot variant="outlined" color={index % 2 == 0 ? "primary" : "success"} />
+                  <TimelineConnector style={{ background: `${index % 2 == 0 ? "#1976d2" : "#2e7d32"}` }} />
+                </TimelineSeparator>
+
+                <TimelineOppositeContent>
+                  <Card className={index % 2 == 0 ? 'align-items-start' : 'align-items-end'}>
+                    <Card.Title>
+                      {item?.studyType}
+                    </Card.Title>
+                    <Card.Text>
+                      {item?.startDate} - {item?.endDate}
+                    </Card.Text>
+                  </Card>
+                </TimelineOppositeContent>
+              </TimelineItem>
+            </>
+          )
+        })}
+      </Timeline>
     </>
   )
 }
